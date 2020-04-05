773d45092982418b7fe026678fb637c6035478b1
Phatcharaphan ananpreechakun <phat.pan@gmail.com>
2020-04-05 21:07:11 +0700

    change password

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :covid, Covid.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "mysecretpassword",
   database: "covid_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
